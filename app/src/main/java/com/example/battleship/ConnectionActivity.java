65874b08b957f194b39eed3f3c2f3ca537357e95
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-10-31 21:22:11 +0300

    New activity for creation connection between users

---
+++ app/src/main/java/com/example/battleship/ConnectionActivity.java
@@ -0,0 +1,15 @@
+package com.example.battleship;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConnectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection);
+    }
+
+}

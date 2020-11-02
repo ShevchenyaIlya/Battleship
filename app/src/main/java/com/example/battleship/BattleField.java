36476b2f06a9393e85736ea20ed38f777c0f4cbe
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-11-03 00:30:19 +0300

    Change structure and delete repeted code by creating new functions.

--- app/src/main/java/com/example/battleship/BattleField.java
+++ app/src/main/java/com/example/battleship/BattleField.java
@@ -2,18 +2,11 @@ package com.example.battleship;
 
 import android.util.Pair;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 
 public class BattleField implements Serializable {
@@ -44,165 +37,84 @@ public class BattleField implements Serializable {
         return vesselCounts;
     }
 
-    public boolean setVessel(int i, int j, boolean orientation, String vesselType)
+    private ArrayList<Pair<Integer, Integer>> getBorderCellsHorizontal(int deckCount, int i, int j)
     {
         ArrayList<Pair<Integer, Integer>> borderCoordinates = new ArrayList<Pair<Integer, Integer>>();
+        int[] array = {-1, 0, 1};
+        for (int value: array)
+        {
+            borderCoordinates.add(new Pair<>(i + value, j - 1));
+        }
+        for (int value: array)
+        {
+            borderCoordinates.add(new Pair<>(i + value, j + deckCount));
+        }
+
+        for (int l = deckCount - 1; l >= 0; l--)
+        {
+            borderCoordinates.add(new Pair<>(i - 1, j + l));
+            borderCoordinates.add(new Pair<>(i + 1, j + l));
+        }
+        return borderCoordinates;
+    }
+
+    private ArrayList<Pair<Integer, Integer>> getVesselCoordinates(int i, int j, int deckCount, boolean orientation)
+    {
         ArrayList<Pair<Integer, Integer>> vesselCoordinates = new ArrayList<Pair<Integer, Integer>>();
+        if (orientation) {
+            for (int l = deckCount - 1; l >= 0; l--) {
+                vesselCoordinates.add(new Pair<>(i, j + l));
+            }
+        }
+        else {
+            for (int l = deckCount - 1; l >= 0; l--) {
+                vesselCoordinates.add(new Pair<>(i + l, j));
+            }
+        }
+        return vesselCoordinates;
+    }
+
+    public boolean setVessel(int i, int j, boolean orientation, String vesselType)
+    {
+        ArrayList<Pair<Integer, Integer>> borderCoordinates;
+        ArrayList<Pair<Integer, Integer>> vesselCoordinates;
         int deckNumber = 0;
         switch (vesselType)
         {
             case "Single-deck":
             {
-                vesselCoordinates.add(new Pair<>(i, j));
                 deckNumber = 1;
-
-                borderCoordinates.add(new Pair<>(i + 1, j + 1));
-                borderCoordinates.add(new Pair<>(i + 1, j));
-                borderCoordinates.add(new Pair<>(i + 1, j - 1));
-                borderCoordinates.add(new Pair<>(i, j - 1));
-                borderCoordinates.add(new Pair<>(i, j + 1));
-                borderCoordinates.add(new Pair<>(i - 1, j + 1));
-                borderCoordinates.add(new Pair<>(i - 1, j));
-                borderCoordinates.add(new Pair<>(i - 1, j - 1));
             }
             break;
             case "Double-deck":
             {
                 deckNumber = 2;
-                if (orientation) {
-                    vesselCoordinates.add(new Pair<>(i, j));
-                    vesselCoordinates.add(new Pair<>(i, j + 1));
-
-                    borderCoordinates.add(new Pair<>(i + 1, j + 2));
-                    borderCoordinates.add(new Pair<>(i + 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 1, j));
-                    borderCoordinates.add(new Pair<>(i + 1, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j + 2));
-
-                    borderCoordinates.add(new Pair<>(i - 1, j + 2));
-                    borderCoordinates.add(new Pair<>(i - 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i - 1, j));
-                    borderCoordinates.add(new Pair<>(i - 1, j - 1));
-                }
-                else {
-                    vesselCoordinates.add(new Pair<>(i, j));
-                    vesselCoordinates.add(new Pair<>(i + 1, j));
-
-                    borderCoordinates.add(new Pair<>(i - 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i - 1, j));
-                    borderCoordinates.add(new Pair<>(i - 1, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 1, j - 1));
-                    borderCoordinates.add(new Pair<>(i + 1, j + 1));
-
-                    borderCoordinates.add(new Pair<>(i + 2, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 2, j));
-                    borderCoordinates.add(new Pair<>(i + 2, j - 1));
-                }
             }
             break;
             case "Three-decker":
             {
                 deckNumber = 3;
-                if (orientation) {
-                    vesselCoordinates.add(new Pair<>(i, j));
-                    vesselCoordinates.add(new Pair<>(i, j + 1));
-                    vesselCoordinates.add(new Pair<>(i, j + 2));
-
-                    borderCoordinates.add(new Pair<>(i + 1, j + 3));
-                    borderCoordinates.add(new Pair<>(i + 1, j + 2));
-                    borderCoordinates.add(new Pair<>(i + 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 1, j));
-                    borderCoordinates.add(new Pair<>(i + 1, j - 1));
-
-                    borderCoordinates.add(new Pair<>(i, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j + 3));
-
-                    borderCoordinates.add(new Pair<>(i - 1, j + 3));
-                    borderCoordinates.add(new Pair<>(i - 1, j + 2));
-                    borderCoordinates.add(new Pair<>(i - 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i - 1, j));
-                    borderCoordinates.add(new Pair<>(i - 1, j - 1));
-                }
-                else {
-                    vesselCoordinates.add(new Pair<>(i, j));
-                    vesselCoordinates.add(new Pair<>(i + 1, j));
-                    vesselCoordinates.add(new Pair<>(i + 2, j));
-
-                    borderCoordinates.add(new Pair<>(i - 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i - 1, j));
-                    borderCoordinates.add(new Pair<>(i - 1, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 1, j - 1));
-                    borderCoordinates.add(new Pair<>(i + 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 2, j - 1));
-                    borderCoordinates.add(new Pair<>(i + 2, j + 1));
-
-                    borderCoordinates.add(new Pair<>(i + 3, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 3, j));
-                    borderCoordinates.add(new Pair<>(i + 3, j - 1));
-                }
             }
             break;
             case "Four-decker":
             {
                 deckNumber = 4;
-                if (orientation) {
-                    vesselCoordinates.add(new Pair<>(i, j));
-                    vesselCoordinates.add(new Pair<>(i, j + 1));
-                    vesselCoordinates.add(new Pair<>(i, j + 2));
-                    vesselCoordinates.add(new Pair<>(i, j + 3));
-
-                    borderCoordinates.add(new Pair<>(i + 1, j + 4));
-                    borderCoordinates.add(new Pair<>(i + 1, j + 3));
-                    borderCoordinates.add(new Pair<>(i + 1, j + 2));
-                    borderCoordinates.add(new Pair<>(i + 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 1, j));
-                    borderCoordinates.add(new Pair<>(i + 1, j - 1));
-
-                    borderCoordinates.add(new Pair<>(i, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j + 4));
-
-                    borderCoordinates.add(new Pair<>(i - 1, j + 4));
-                    borderCoordinates.add(new Pair<>(i - 1, j + 3));
-                    borderCoordinates.add(new Pair<>(i - 1, j + 2));
-                    borderCoordinates.add(new Pair<>(i - 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i - 1, j));
-                    borderCoordinates.add(new Pair<>(i - 1, j - 1));
-                }
-                else {
-                    vesselCoordinates.add(new Pair<>(i, j));
-                    vesselCoordinates.add(new Pair<>(i + 1, j));
-                    vesselCoordinates.add(new Pair<>(i + 2, j));
-                    vesselCoordinates.add(new Pair<>(i + 3, j));
-
-                    borderCoordinates.add(new Pair<>(i - 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i - 1, j));
-                    borderCoordinates.add(new Pair<>(i - 1, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j - 1));
-                    borderCoordinates.add(new Pair<>(i, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 1, j - 1));
-                    borderCoordinates.add(new Pair<>(i + 1, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 2, j - 1));
-                    borderCoordinates.add(new Pair<>(i + 2, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 3, j - 1));
-                    borderCoordinates.add(new Pair<>(i + 3, j + 1));
-
-                    borderCoordinates.add(new Pair<>(i + 4, j + 1));
-                    borderCoordinates.add(new Pair<>(i + 4, j));
-                    borderCoordinates.add(new Pair<>(i + 4, j - 1));
-                }
             }
             break;
+        }
+
+        vesselCoordinates = getVesselCoordinates(i, j, deckNumber, orientation);
+        if (orientation) {
+            borderCoordinates = getBorderCellsHorizontal(deckNumber, i, j);
+        }
+        else {
+            borderCoordinates = getBorderCellsVertical(deckNumber, i, j);
         }
 
         boolean fail = false;
         for (int l = 0; l < borderCoordinates.size(); l++)
         {
-            if (!checkIfCorrect(borderCoordinates.get(l).first, borderCoordinates.get(l).second, orientation))
+            if (!checkIfCorrect(borderCoordinates.get(l).first, borderCoordinates.get(l).second))
             {
                 fail = true;
                 break;
@@ -232,7 +144,12 @@ public class BattleField implements Serializable {
         return field[i][j] == 1;
     }
 
-    private boolean checkIfCorrect(int i, int j, boolean orientation)
+    public int isAttacked(int i, int j)
+    {
+        return field[i][j];
+    }
+
+    private boolean checkIfCorrect(int i, int j)
     {
         if ((i >= 0 && i < 10) && (j >= 0 && j < 10)) {
             return field[i][j] != 1;
@@ -278,13 +195,71 @@ public class BattleField implements Serializable {
         return Arrays.asList(field[index]);
     }
 
-    public Map<String, Object> getFieldAsMap(User user)
+    public int opponentAttack(int i, int j)
+    {
+        if (field[i][j] == 1) {
+            field[i][j] = 3;
+            return 1;
+        }
+        else if (field[i][j] == 0) {
+            field[i][j] = 2;
+            return 0;
+        }
+        return 2;
+    }
+
+    public Map<String, Object> getFieldAsMap()
     {
         Map<String, Object> data = new HashMap<>();
-        data.put("user", user.getEmail());
         for (int i = 0; i < 10; i++) {
             data.put("row " + i, serialize(i));
         }
         return data;
+    }
+
+    public void setFieldFromMap(Map<String, Object> data)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            ArrayList<Long> row = (ArrayList<Long>)data.get("row " + i);
+            for (int j = 0; j < 10; j++) {
+                Integer value = row.get(j).intValue();
+                field[i][j] = value;
+            }
+        }
+    }
+
+    public boolean isAllVesselsKilled()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (field[i][j] == 1)
+                    return false;
+            }
+        }
+    return true;
+    }
+
+    private ArrayList<Pair<Integer, Integer>> getBorderCellsVertical(int deckCount, int i, int j)
+    {
+        ArrayList<Pair<Integer, Integer>> borderCoordinates = new ArrayList<Pair<Integer, Integer>>();
+        int[] array = {-1, 0, 1};
+        for (int value: array)
+        {
+            borderCoordinates.add(new Pair<>(i - 1, j + value));
+        }
+        for (int value: array)
+        {
+            borderCoordinates.add(new Pair<>(i + deckCount, j + value));
+        }
+
+        for (int l = deckCount - 1; l >= 0; l--)
+        {
+            borderCoordinates.add(new Pair<>(i + l, j - 1));
+            borderCoordinates.add(new Pair<>(i + l, j + 1));
+        }
+        return borderCoordinates;
     }
 }

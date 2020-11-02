539a67edf18d455582efcc31a0d85c9df9a2ddfc
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-11-03 00:28:29 +0300

    Updated user class by extending structure with new fields

--- app/src/main/java/com/example/battleship/User.java
+++ app/src/main/java/com/example/battleship/User.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 public class User implements Serializable {
     private String email;
     private String password;
+    private String status;
+    private String connectionId;
+    private String nickname;
 
     public User() { }
 
@@ -27,5 +30,29 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getConnectionId() {
+        return connectionId;
+    }
+
+    public void setConnectionId(String connectionId) {
+        this.connectionId = connectionId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 }

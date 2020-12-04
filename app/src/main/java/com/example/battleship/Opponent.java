373d00d9fb4bdd0812af3e1957b09baf47ab1a6d
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-12-04 23:01:29 +0300

    Incapsulate opponent data to class.

---
+++ app/src/main/java/com/example/battleship/Opponent.java
@@ -0,0 +1,22 @@
+package com.example.battleship;
+
+public class Opponent {
+    private String opponentEmail;
+    private String opponentId;
+
+    public String getOpponentEmail() {
+        return opponentEmail;
+    }
+
+    public void setOpponentEmail(String opponentEmail) {
+        this.opponentEmail = opponentEmail;
+    }
+
+    public String getOpponentId() {
+        return opponentId;
+    }
+
+    public void setOpponentId(String opponentId) {
+        this.opponentId = opponentId;
+    }
+}
